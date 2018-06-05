b224671eb9447914df58423622cc25092f1ffe34
bogdantoma27 <39964702+bogdantoma27@users.noreply.github.com>
2018-06-05 20:45:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chimie-project-2013
---
+++ js/googleMap.js
@@ -0,0 +1,55 @@
+$(window).load(function(){
+	var mapUrl = "https://maps.google.ro/maps?ie=UTF8&cid=10257043351588580986&q=Colegiul+Na%C5%A3ional+Liceal+%22Alexandru+Odobescu%22&iwloc=A&gl=RO&hl=ro",
+ 		onLoadWebSite = false,
+   		googleMapHolder = $(".google_map"),
+        backgroundColor = googleMapHolder.css("backgroundColor"),
+        mapWidth=googleMapHolder.css("width"),
+        mapHeight=googleMapHolder.css("height"),
+        borderTopLeftRadius = googleMapHolder.css("borderTopLeftRadius"),
+        borderTopRightRadius = googleMapHolder.css("borderTopLeftRadius"),
+        borderBottomLeftRadius = googleMapHolder.css("borderTopLeftRadius"),
+        borderBottomRightRadius = googleMapHolder.css("borderTopLeftRadius"),
+        addMap=false,
+		idPage,
+		intervalCall;
+	    
+    if(backgroundColor == "rgba(0, 0, 0, 0)"){
+        backgroundColor= "#ffffff";
+    }
+    verificationPageHandler();
+    if(onLoadWebSite == false){
+        $(window).bind("hashchange", verificationPageHandler);
+    }
+    function verificationPageHandler(){
+        if(onLoadWebSite == false){
+        	idPage = "#"+window.location.hash.substring(3, window.location.hash.length);
+        	if(idPage != "#"){
+				if(googleMapHolder.parents(idPage).length != 0){
+	                addGoogleMapHandler();
+       			}	
+        	}
+        }else{
+            addGoogleMapHandler();
+        }
+    }
+    function addGoogleMapHandler(){
+        if($('#loaderPart').length == 0){
+			$(window).unbind("hashchange", verificationPageHandler);
+            googleMapHolder.css({"overflow":"hidden"});
+            googleMapHolder.append("<div id='loaderPart' style='position:absolute; z-index:1; width:"+mapWidth+"; height:"+mapHeight+"; background:"+backgroundColor+" url(images/googleMapLoader.gif) no-repeat 50%; border-top-left-radius:"+borderTopLeftRadius+"; border-top-right-radius:"+borderTopRightRadius+"; border-bottom-right-radius:"+borderBottomLeftRadius+"; border-bottom-left-radius:"+borderBottomRightRadius+";'></div>");
+            intervalCall = setInterval(addIframe, 200)
+        }
+        function addIframe(){
+        	if($(idPage).css("display")!="none"){
+        		clearInterval(intervalCall);
+	     	  	googleMapHolder.append("<iframe width='"+mapWidth+"' height='"+mapHeight+"' frameborder='0' src='"+mapUrl+"' style='position:absolute; z-index:0; border-top-left-radius:"+borderTopLeftRadius+"; border-top-right-radius:"+borderTopRightRadius+"; border-bottom-right-radius:"+borderBottomLeftRadius+"; border-bottom-left-radius:"+borderBottomRightRadius+";'></iframe>");
+	        	googleMapHolder.find("iframe").css({"visibility":"hidden"}).load(googleMapLoadCompleteHandler);
+			}
+        }
+    }
+    function googleMapLoadCompleteHandler(){
+    	googleMapHolder.find("iframe").css({"visibility":"visible"})
+    	var loaderPart = googleMapHolder.find("#loaderPart");
+        loaderPart.delay(100).fadeOut(500, function(){loaderPart.css({"display":"none"});});
+    }
+})
